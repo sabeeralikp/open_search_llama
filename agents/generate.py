765a6dea7edbfcbbfdae5c150420a5b953d23a51
sabeeralikp <sabeeralikp8@gmail.com>
2024-11-10 19:41:24 +0530

    changes in Generate Prompt

--- agents/generate.py
+++ agents/generate.py
@@ -12,7 +12,7 @@ generate_prompt = PromptTemplate(
     
     You are an AI assistant for Web Search, that synthesizes web search results. 
     Strictly use the following pieces of web search context to answer the question. If you don't know the answer, just say that you don't know. 
-    keep the answer concise, but provide all of the details you can in the form of a report, Cite the sources also. 
+    keep the answer concise, but provide all of the details you can in the form of a report. 
     Only make direct references to material if provided in the context.
     
     <|eot_id|>
